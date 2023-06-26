14acecb71eff8c506ab7048f22c0b6b25667e817
trevo <trevorius45@gmail.com>
2023-06-26 17:13:29 -0400

    accept api-key

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,16 @@
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
+
+  const apiKey = process.env.API_KEY;
+  const token = req.headers['api-key'];
+
+  if (apiKey === token) {
+    return next();
+  } else if (req.isAuthenticated()) {
     return next();
   } else {
     // User is not authenticated, redirect to login page
-    res.redirect('/login');
+    // res.redirect('/login');
+    res.status(403).send('not logged in ');
   }
 }
 
